fe3ca73f3b2df762c6194493e276005a467c03b0
sameer khan <sameerkhanaa5@gmail.com>
2024-01-22 21:05:57 +0530

    DB and status code

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,25 @@
+const { BadRequestError } = require("../errors");
+const User = require("../models/User");
 
-const register = async (req,res)=>{
-  res.send('register user')
-}
-const login = async (req,res)=>{
-  res.send('login user')
-}
+const StatusCodes = require("http-status-codes");
 
+const bcryptjs = require("bcryptjs");
 
-module.exports = {register , login}
+const register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const hashPassword = await bcryptjs.hash(password, 10);
+
+  const tempUser = { name, email, password: hashPassword };
+
+  const user = await User.create(tempUser);
+  console.log(user);
+
+  res.json(user);
+  // res.status(StatusCodes.CREATED).json(user);
+};
+const login = async (req, res) => {
+  res.send("login user");
+};
+
+module.exports = { register, login };
